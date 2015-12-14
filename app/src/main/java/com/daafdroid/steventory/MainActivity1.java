e609f5afb9c5a2997b7cbf305d8a328faa4fed96
David <david_leffers@live.nl>
2015-12-15 00:05:02 +0100

    Deleted some unnecessary stuff
    unnecessary

--- app/src/main/java/com/daafdroid/steventory/MainActivity1.java
+++ app/src/main/java/com/daafdroid/steventory/MainActivity1.java
@@ -5,7 +5,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
@@ -23,9 +22,6 @@ public class MainActivity1 extends ListActivity {
         DBHandler myDBHandler=new DBHandler(this, null, null, 1);
         database = myDBHandler.getWritableDatabase();
 
-        //Get the ListView to populate
-        ListView listViewToPopulate = getListView();
-
         //Fill cursor with some data
         Cursor dataCursor = database.rawQuery("SELECT * FROM products", null);
 
@@ -37,7 +33,7 @@ public class MainActivity1 extends ListActivity {
                 myDBHandler.ROW_QUANTITY,
         };
 
-        //the XML defined views which the data will be bound to (Defined in entry.xml)
+        //the XML defined views to populate with our data (Defined in entry.xml)
         int[] to = new int[] {
                 R.id.productName,
                 R.id.manufacturer,
@@ -47,13 +43,11 @@ public class MainActivity1 extends ListActivity {
         //Create SimpleCursorAdapter to populate ListView with database records
         cursorAdapter = new SimpleCursorAdapter(this, R.layout.entry, dataCursor, columns, new int[]{R.id.productName, R.id.manufacturer, R.id.stockAmount}, 0);
 
-        listViewToPopulate.setHeaderDividersEnabled(true);
-
         setListAdapter(cursorAdapter);
     }
 
     public void addNewProduct(View view) {
-    //This method is called when button with id "btn_" is clicked
+    //This method is called when button with id "btn_new_product" is clicked
 
         Toast.makeText(MainActivity1.this, "BUTTON NEW PRODUCT CLICKED", Toast.LENGTH_SHORT).show();
     }
